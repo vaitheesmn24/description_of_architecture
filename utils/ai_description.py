96ef43e062f3296bf11353da875fba40b1c8dd3e
schsiddu9575 <schsiddu9575@gmail.com>
2023-11-01 17:36:42 +0530

    updated des

--- utils/ai_description.py
+++ utils/ai_description.py
@@ -1,9 +1,7 @@
 import openai
 import os
 
-# openai.api_key=os.getenv("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")
 
-# openai.api_key = "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"
 openai.api_key="open-api-key"
 
 def create_prompt(list_of_services):
@@ -15,9 +13,7 @@ def create_prompt(list_of_services):
 def description_prompt(text):
     
     description_prompt = create_prompt([f"{text}"])
-    # description_prompt = create_prompt([text])
     
-    # print(description_prompt,'--------------------------------------------------------')
 
     response = openai.Completion.create(engine="text-davinci-003",
                                                 prompt= description_prompt,
@@ -27,14 +23,6 @@ def description_prompt(text):
     formatted_response=response["choices"][0]["text"]
     
     
-    # first_part = formatted_response.split("The architectural flow")[0]
-    # second_part=formatted_response.split("The architectural flow")[1]
-    
-    # formatted_flow=first_part.strip()
-    # formatted_res=second_part.strip()
-    
-    # complete_des=f"{formatted_flow} \n {formatted_res}"
-        
     
     return formatted_response
     
